d990430ae07d1914dc91099672f5fca49b1e2749
floralbarello <floralbarello@gmail.com>
2020-06-28 21:30:53 -0300

    archivo-ejemplo

---
+++ archivo-homework2.txt.js
@@ -0,0 +1,6 @@
+
+Variables: Una variables es un nombre que utilizo para representar un valor posible de un dato que utilizare durante la programacion.  
+Strings: Es un tipo de variable. Los datos "strings" son cadenas de caracteres. Si los caracteres son unicamente letras, se formara una palabra.  
+Funciones (argumentos, return): Son conjuntos de operaciones que puedo guardar en conjunto. Al llamar a la funcion durante la programacion, solo necesitare especificar con que datos quiero que realice las operaciones (argumentos). Con la declaracion "return" puedo hacer que la funcion me devuelva un resultado. 
+Declaraciones 'if': El operador "if" se utiliza para verificar si algo es verdadero o falso. Si se cumple ese "algo", se ejecuta la declaracion "return" especificada. Si no se cumple y no se determino que hacer en caso de que sea falso, finaliza el control de flujo. 
+Valores booleanos ('true', 'false'): Son otro tipo de variables, pero unicamente pueden tomar el valor "true" o "false". 
